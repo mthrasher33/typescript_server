d7fbe43aac01ee270899db585b03abe025e4f45e
Matt Thrasher <matthew.j.thrasher@gmail.com>
2022-02-01 17:05:46 -0600

    Basic login form

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hi there');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
